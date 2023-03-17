d84be1809c8c6f4d36736ba9d1ad5bb8b86c1240
FlavaDave92 <117933026+FlavaDave92@users.noreply.github.com>
2023-03-17 10:21:58 -0700

    Update producer_consumer.c

--- producer_consumer.c
+++ producer_consumer.c
@@ -12,7 +12,7 @@
 
 int producer_consumer_init(int buffSize, int prod, int cons, int uuid)
 {
-    module_param(uid, buff_size, p, c) #uid is user uid, buffer size, total # of producer threads, total # of consumer threads
+    module_param(buffSize, prod, cons, uuid)
     printk(KERN_INFO "Hello world\n");
     return 0;
 }
@@ -20,8 +20,10 @@ int producer_consumer_init(int buffSize, int prod, int cons, int uuid)
 void producer_consumer_exit(void)
 {
     down_interruptible(mutex);
-    down_interruptible();
-    down_interruptible();
+    down_interruptible(full);
+    down_interruptible(empty);
+    if buffer_index < 0
+        
     kthread_stop(struct task_struct *producer);
     kthread_stop(struct task_struct *consumer);
     printk(KERN_INFO "The total elapsed time of all processes for UID %d is", uuid);
